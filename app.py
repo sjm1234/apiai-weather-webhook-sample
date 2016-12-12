f4db2bd6d36ec3f8970ac3040f60f12139bfca3c
sjm1234 <simon.j.moroney@gmail.com>
2016-12-12 12:57:23 +1100

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def makeWebhookResult(data):
 
     # print(json.dumps(item, indent=4))
 
-    speech = "Simon is great. Today in " + location.get('city') + ": " + condition.get('text') + \
+    speech = "Simon is great yes. Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
