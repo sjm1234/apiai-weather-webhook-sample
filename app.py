48ef69cc2155f993d4b21893ad1b6fedd0c100c9
sjm1234 <simon.j.moroney@gmail.com>
2016-12-14 12:55:13 +1100

    Update app.py

--- app.py
+++ app.py
@@ -20,13 +20,11 @@ def webhook():
     print(json.dumps(req, indent=4))
     
   
-    url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=-33.818961&lng=151.105809&fDstL=0&fDstU=1"
-    response = urllib.urlopen(url)
-    planeData = json.loads(response.read())
+    #url = "https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=-33.818961&lng=151.105809&fDstL=0&fDstU=1"
+    #response = urllib.urlopen(url)
+    #planeData = json.loads(response.read())
     
-    #req = urllib2.Request('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=-33.818961&lng=151.105809&fDstL=0&fDstU=1')
-    #respoe = urllib2.urlopen(req)
-    #the_page = response4.read()
+
     
 
     
