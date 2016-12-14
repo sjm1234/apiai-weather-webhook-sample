ea7108ffcdbbb4935eb3419d6fc7f2ee4b07a206
sjm1234 <simon.j.moroney@gmail.com>
2016-12-14 14:47:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def webhook():
     
 
     
-    req = urllib.request.Request('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=-33.818961&lng=151.105809&fDstL=0&fDstU=1')
+   # req = urllib.request.Request('https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=-33.818961&lng=151.105809&fDstL=0&fDstU=1')
     
 
     res = {
