a0fba4f60bf53a4f767e245066f2bc64aeef680c
sjm1234 <simon.j.moroney@gmail.com>
2016-12-14 17:03:20 +1100

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def webhook():
     readdata = reqq.read()
     
     planedata = json.loads('{"src":1,"feeds":[{"id":1,"name":"From Consolidator","polarPlot":false}],"srcFeed":1,"showSil":true,"showFlg":true,"showPic":true,"flgH":20,"flgW":85,"acList":[],"totalAc":3292,"lastDv":"636172813226560026","shtTrlSec":65,"stm":1481692022571}')
-    
+    sampletext = planedata["src"]
     speech = "Today in"
 
     res = {
